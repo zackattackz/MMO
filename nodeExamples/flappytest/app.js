5dbdc29bfd707ab89a5abf5968d8cac61ceb85cb
Zachary Hanham <z.hanham00@gmail.com>
2019-02-26 20:12:53 -0500

    phaser + sockets example

---
+++ nodeExamples/flappytest/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + "/public" ));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', socket => {
+    console.log('User connected');
+
+    socket.on('position', pos => {
+        console.log("My position is x: " + pos.x + "\ny: " + pos.y);
+    });
+});
+
+http.listen(3000, function() {
+    console.log("listening on *:3000");
+});
---
+++ nodeExamples/phaserTest/public/game.js
@@ -0,0 +1,17 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y : 200}
+        }
+    },
+    scene: [Example1]
+};
+
+var socket = io();
+console.log(socket);
+
+var game = new Phaser.Game(config);
