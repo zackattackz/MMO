51da24be234f556548d5920e3ac3e6be419ad525
Zachary Hanham <z.hanham00@gmail.com>
2019-03-05 13:32:06 -0500

    added menu scene

---
+++ public/js/scenes/menu.js
@@ -0,0 +1,19 @@
+class menu extends Phaser.Scene {
+    constructor(){
+        super({key: "mainScene"});
+    }
+
+    preload(){
+
+    }
+
+    create() {
+
+
+
+    }
+
+    update(dt) {
+
+    }
+}
