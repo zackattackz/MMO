29077e9bc0e27303bcf9ce78f3e4d7cad92c1e93
Zachary Hanham <z.hanham00@gmail.com>
2019-03-08 13:08:20 -0500

    Refactored and commented findWinner

--- js/gameState.js
+++ js/gameState.js
@@ -36,18 +36,20 @@ let gameState = {
 
     findWinner: function() { //will search through each player and check if only one is active, if so return that player
                              //id else return empty string
+
+        //If there's 1 or 0 players, there can't be a winner so return ""
         if (this.getPlayerCount() <= 1) return "";
-        const playerIds = Object.keys(this.state.players);
+        const players = Object.values(this.state.players);//Get all players
         let activePlayers = 0;
         let winnerId = "";
 
-        for(let id of playerIds) {
-            if (this.state.players[id].isActive) {
+        for(let player of players) {//for each player
+            if (player.isActive) {
                 activePlayers++;
-                winnerId = id;
+                winnerId = player.id;//If there is only 1 active player then winnerId will become the id of that player
             }
         }
-
+        //if there are more than 1 active players, return "" because no winner, else return that player's id
         return activePlayers === 1 ? winnerId : "";
 
     }
