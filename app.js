cf1bebaac9c963bbb8b1e7584e5345767fb0fc0c
Zachary Hanham <39349995+zackattackz@users.noreply.github.com>
2019-04-19 22:00:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ let server = require('http').Server(app);
 let io = require('socket.io').listen(server);
 let db = require('./db');
 
-db.con.connect();
-
 let gameState = require("./js/gameState");
 
 app.use(express.static(__dirname + '/public'));
@@ -174,4 +172,25 @@ db.getHighScoreObject(highScoreObject => {
     startPlayerCheckInterval();
 });
 
+function handleDisconnect() {
+                                                  // the old one cannot be reused.
+
+  db.con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  db.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
